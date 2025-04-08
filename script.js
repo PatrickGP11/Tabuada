f8b854612e0fd0165183c0f243c76dad1d3cc727
PatrickGP11 <patrickgonsalves06@gmail.com>
2025-04-08 18:37:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-var body = document.querySelector("body")
-var numero = Number(prompt("Digite um numero"))
-for(var i = 1; i <= 10; i++) {
-    body.innerHTML += `<p>${numero} x ${i} = ${numero * i}</p>`
+var btn = document.querySelector("#btn")
+var inputNum = document.querySelector("#num")
+
+function tabuada() {
+    var res = document.querySelector(".res")
+    var numTabuada = inputNum.value
+    res.innerHTML = ""
+
+    for(var inicio = 1; inicio <= 10; inicio++) {
+        res.innerHTML += `<p>${numTabuada} x ${inicio} = ${numTabuada * inicio}</p>`
+    }
 }
+
+btn.addEventListener("click", tabuada)
